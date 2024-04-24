bf63b1e6c27ac57f3a1f40fc9c469d1929a1e2cc
Urfenox <elcrismcmcelcris@gmail.com>
2024-04-23 23:37:28 -0400

    navbar mejoras
    mensajes agregados
    
    edicion agregada
    
    listado de tickets en home agregado

--- Tickets/Tickets/settings.py
+++ Tickets/Tickets/settings.py
@@ -131,3 +131,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
--- Tickets/ticketing/views.py
+++ Tickets/ticketing/views.py
@@ -1,23 +1,41 @@
-from django.shortcuts import render, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TicketForm
+from .models import Ticket
+from django.contrib import messages
 
 # Create your views here.
 
 def home(respuesta):
-    return render(respuesta, "ticketing/home.html", {})
+    tickets = Ticket.objects.all()
+    return render(respuesta, "ticketing/home.html", {"tickets":tickets})
 
 def create(respuesta):
+    form = TicketForm()
     if respuesta.method == "POST":
         form = TicketForm(respuesta.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/tickets")
-    else:
-        form = TicketForm()
+            messages.success(respuesta, '¡Ticket creado correctamente!')
+            return redirect("home")
+        else:
+            messages.error(respuesta, form.errors)
     return render(respuesta, "ticketing/create.html", {"form":form})
 
 def edit(respuesta, id):
-    return render(respuesta, "ticketing/edit.html", {"id": id})
+    ticket = get_object_or_404(Ticket, id=id) # pk=id o id=id funcionan | si no existe la ID, 404
+    form = TicketForm(respuesta.POST or None, instance=ticket)
+    if respuesta.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(respuesta, '¡Ticket editado correctamente!')
+        return redirect("home")
+    else:
+        messages.error(respuesta, form.errors)
+    return render(respuesta, "ticketing/edit.html", {"ticket":ticket ,"form": form})
 
 def delete(respuesta, id):
-    return render(respuesta, "ticketing/delete.html", {"id": id})
+    messages.error(respuesta, "Esta funcion no se encuentra habilitada.")
+    return redirect("home")
+    # ticket = get_object_or_404(Ticket, id=id)
+    # ticket.delete()
+    # messages.success(respuesta, '¡Ticket eliminado correctamente!')
+    # return redirect("home")
